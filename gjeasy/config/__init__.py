c1cd36e990839a860a76d8bce21e55b9266ec237
jamomhe <352622654@qq.com>
2014-02-17 19:23:12 +0800

    add log

---
+++ gjeasy/config/__init__.py
@@ -0,0 +1,5 @@
+#coding=utf8
+
+import os
+
+current_env = os.environ.get("GJEASY_ENV") or "development"
--- gjeasy/config/configure.py
+++ gjeasy/config/configure.py
@@ -1,6 +1,11 @@
 #coding=utf8
 import os
+from gjeasy.config import current_env
+
 LOG_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "log")
 
 LOG_LEVEL = "debug"
-LOG_FILE = "/home/easy/log/gjeasy/gjeasy.log"
+if current_env == "development":
+    LOG_FILE = "%s/gjeasy.log" % LOG_PATH
+else:
+    LOG_FILE = "/home/easy/log/gjeasy/gjeasy.log"
