7f1bf82fb28a75299b74c7ee4ff3150b45225df8
jamonhe <352622654@qq.com>
2014-03-08 00:53:59 +0800

    add period celery

--- gjeasy/tasks/period_task.py
+++ gjeasy/tasks/period_task.py
@@ -1,12 +1,12 @@
 #coding=utf8
-from gjeasy.emails import send_mail
-from gjeasy.controls.get_msg import get_msg
+from gjeasy.config.celeryconfig import celery
+from gjeasy.controls.daemon_main import daemon_main
+
 
 
 key_word = [u"姚贝娜"]
 test_email = "352622654@qq.com"
 
-def grab_msg(keywords=key_word, name=u"姚贝娜", interval=2000, to_mail=test_email):
-    send_contents = get_msg(keywords=keywords, name=name, interval=interval)
-    if send_contents:
-        send_mail(test_email, "gjeasy grab info", send_contents)
+@celery.task(ignore_result=True)
+def grab_msg(keywords=key_word, name=u"姚贝娜", interval=300, to_mail=test_email):
+    daemon_main()
